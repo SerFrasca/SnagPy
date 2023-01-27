28b811417ac74af991f17ce804ac0c38156c0126
SerFrasca <119513685+SerFrasca@users.noreply.github.com>
2023-01-27 10:54:01 +0100

    Add files via upload

---
+++ starting.py
@@ -0,0 +1,15 @@
+# copy and paste helper snippets
+
+# initial importing ------------------
+
+import numpy as np
+import GD,BASIC,ML_PY,SERV,STAT,SIGNAL,ASTROTIME,GD2,GWDATA,BSD,GUISNAG
+import importlib
+# importlib.reload(Module)
+
+# symbols -----------------
+
+snagpy_p,gwdata_p,examples_p,exper_p,pers_p,projects_p,\
+antennas_p,cwinj_o2_p,cwinj_o3_p,cwsour_p,\
+table_ligoh_p,table_ligol_p,table_virgo_p,table_kagra_p,de440s_p\
+    =GWDATA.set_symbols()
